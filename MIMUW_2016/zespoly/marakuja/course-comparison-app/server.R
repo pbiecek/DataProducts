8e5be7a413d2ca12055560265c9d040747e5b6d9
SzymonDz <szymon.dziewiatkowski@gmial.com>
2016-04-14 01:38:14 +0200

    Corrected plot

--- MIMUW_2016/zespoly/marakuja/course-comparison-app/server.R
+++ MIMUW_2016/zespoly/marakuja/course-comparison-app/server.R
@@ -10,7 +10,7 @@ shinyServer(function(input, output) {
   })
   
   output$corDiagram = renderPlot(
-    ggplot(points(), aes(x=AVG_GROUP_1, y=AVG_GROUP_2)) + stat_bin2d(bins=6)
+    ggplot(points(), aes(x=AVG_GROUP_1, y=AVG_GROUP_2)) + stat_bin2d(bins=6) + ggtitle("Zależności ocen grup przedmiotów") + xlab("Grupa 1.") + ylab("Grupa 2.") + scale_fill_gradient(low="#eeeeee", high="#000000", guide = "legend") + theme(panel.background = element_rect(fill = "#ffffff"))
   )
   
   output$corTable = renderDataTable(points())
