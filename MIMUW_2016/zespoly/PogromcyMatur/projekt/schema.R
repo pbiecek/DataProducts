0a5e22a8169c61892b7f7f7f0004368dfb61a3d7
Franciszek Piszcz <franek.piszcz@gmail.com>
2016-04-16 13:02:07 +0200

    schema.R - schemat naszej wewnetrznej reprezentacji danych

---
+++ MIMUW_2016/zespoly/PogromcyMatur/projekt/schema.R
@@ -0,0 +1,39 @@
+init <<- function() {
+
+  regiony <<- data.frame()
+  regiony$wojewodztwo <<- character(0)
+  regiony$powiat <<- character(0)
+  regiony$gmina <<- character(0)
+
+  szkoly <<- data.frame()
+  szkoly$id <<- integer(0)
+  szkoly$rok <<- integer(0)
+  szkoly$gmina <<- character(0)
+  szkoly$powiat <<- character(0)
+  szkoly$wojewodztwo <<- character(0)
+
+  typy_testow <<- data.frame()
+  typy_testow$id <<- integer(0)
+  typy_testow$rodzaj_egzaminu <<- character(0)
+  typy_testow$czesc_egzaminu <<- character(0)
+  typy_testow$rok <<- integer(0)
+
+  oceny_szkol <<- data.frame()
+  oceny_szkol$id <<- integer(0)
+  oceny_szkol$id_testu <<- integer(0)
+  oceny_szkol$sredni_wynik <<- integer(0)
+
+  wyniki_szkol <<- data.frame()
+  wyniki_szkol$id <<- integer(0)
+  wyniki_szkol$id_testu <<- integer(0)
+  wyniki_szkol$sredni_wynik <<- integer(0)
+  wyniki_szkol$id_kryterium <<- character(0)
+
+  kryteria <<- data.frame()
+  kryteria$id <<- character(0)
+  kryteria$numer_pytania <<- character(0)
+  kryteria$id_wiazki <<- integer(0)
+  kryteria$numer_kryterium <<- character(0)
+  kryteria$id_testu <<- integer(0)
+  kryteria$max_punktow <<- integer(0)
+}
