fb8ad86f7ecbba9d786cbbd2956800e2980533a8
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2016-04-16 18:20:07 +0200

    Remove old code, add data loading
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- MIMUW_2016/zespoly/PogromcyMatur/projekt/server.R
+++ MIMUW_2016/zespoly/PogromcyMatur/projekt/server.R
@@ -23,44 +23,5 @@ shinyServer(function(input, output) {
   
 })
 
-src = polacz()
-
-szkola_uczniowie <- function(gmina, w_rok, typ, egz_rodz, egz_czesc) {
-  
-  szkoly = pobierz_szkoly(src) %>%
-    filter(gmina_szkoly == gmina, rok == w_rok, typ_szkoly == typ) %>%
-    select(id_szkoly, gmina_szkoly, nazwa_szkoly, rok, wojewodztwo_szkoly, typ_szkoly)
-  
-  szkoly_f = szkoly %>%
-      select(id_szkoly, rok, wojewodztwo_szkoly)
-  
-  ut = pobierz_dane_uczniowie_testy(src) %>%
-    inner_join(szkoly_f)
-  
-  uczniowie = pobierz_uczniow(src) %>%
-    inner_join(ut) %>%
-    select(id_szkoly, id_cke, id_testu, id_obserwacji)
-  
-  dane = pobierz_wyniki_egzaminu(src, egz_rodz, egz_czesc, w_rok, TRUE) %>%
-    inner_join(szkoly_f) %>%
-    inner_join(uczniowie) %>%
-    select(-id_szkoly, -rok, -wojewodztwo_szkoly, -id_testu, -id_obserwacji, -id_cke) %>%
-    collect() %>%
-    summarise_each(funs(mean(., na.rm = TRUE)))
-  return(dane)
-}
-
-pobierz_gminy <- function() {
-  dane = pobierz_szkoly(src) %>%
-    select(gmina_szkoly, wojewodztwo_szkoly) %>%
-    distinct() %>%
-    arrange(wojewodztwo_szkoly, gmina_szkoly) %>%
-    collect()
-  return(dane)
-}  
-
-wyniki_usrednij <- function(wyniki) {
-  res = wyniki %>%
-    group_by(id_testu)
-  return(res)
-}
+load("bigData.RData")
+load("smallData.RData")
