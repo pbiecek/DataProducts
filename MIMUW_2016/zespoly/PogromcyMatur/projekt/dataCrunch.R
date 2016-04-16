904254da9e40b87cc46a0b7c364d6ee1b2c2b36c
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2016-04-16 18:21:18 +0200

    First implementation of data crunching from the server, yet to be tested
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- MIMUW_2016/zespoly/PogromcyMatur/projekt/dataCrunch.R
+++ MIMUW_2016/zespoly/PogromcyMatur/projekt/dataCrunch.R
@@ -1,33 +1,107 @@
 library(ZPD)
 
-src = polacz()
+load("bigData.RData")
+load("smallData.RData")
 
-rocznik <- function (rok){
-  uczniowie = pobierz_uczniow(src) %>%
-    filter(rocznik == rok)
-  ut = pobierz_dane_uczniowie_testy(src) %>%
-    join(uczniowie) %>%
-    select(id_szkoly, id_obserwacji) %>%
-    unique(id_szkoly, id_obserwacji) %>%
-  return(ut)
+zapisz_male_dane <- function() {
+  save(regiony, szkoly, typy_testow, kryteria, file="smallData.RData")
 }
 
-oceny_szkol_po_tescie <- function (id_testu, rok) {
-  wyniki = pobierz_wyniki_testu(src, id_testu) %>%
-    zsumuj_punkty(TRUE)
-  uczniowie = rocznik(rok)
-  os = join(uczniowie, wyniki) %>%
-    select(id_szkoly, wynik) %>%
-    group_by(id_szkoly) %>%
-    summarize(id_szkoly, wynik_szkoly = mean(wynik))
-  return(os)
+zapisz_duze_dane() <- function() {
+  save(oceny_szkol, wyniki_szkol, file="bigData.RData")
 }
 
-wyniki_szkol_z_testu <- function (id_testu) {
-  wyniki = pobierz_odpowiedzi(src) %>%
-    filter(id_testu = id_testu) %>%
-    select(id_szkoly, kryterium, ocena) %>%
-    group_by(id_szkoly, kryterium) %>%
-    summarize(id_szkoly,kryterium, wynik_szkoly=mean(wynik))
-  return(wyniki)
+przeladuj_regiony <- function() {
+  src = polacz()
+  reg = pobierz_szkoly(src) %>%
+        select(gmina_szkoly, powiat_szkoly, wojewodztwo_szkoly) %>%
+        unique(gmina_szkoly, powiat_szkoly, wojewodztwo_szkoly) %>%
+        rename(gmina = gmina_szkoly, powiat = powiat_szkoly, wojewodztwo = wojewodztwo_szkoly) %>%
+        collect() %>%
+        as.data.frame()
+  regiony <<- reg
+}
+
+zaladuj_nowe_szkoly <- function(rok) {
+  src = polacz()
+  nowe = pobierz_szkoly(src) %>%
+         filter(rok == rok) %>%
+         select(id_szkoly, gmina_szkoly, powiat_szkoly, wojewodztwo_szkoly, nazwa_szkoly, typ_szkoly, rok) %>%
+         rename(id = id_szkoly, gmina = gmina_szkoly, powiat = powiat_szkoly, wojewodztwo = wojewodztwo_szkoly,
+                nazwa = nazwa_szkoly, typ = typ_szkoly) %>%
+         collect() %>%
+         as.data.frame()
+  szkoly <<- rbind(szkoly, nowe)
+}
+
+zaladuj_nowe_testy <- function(rok) {
+  src = polacz()
+  nowe = pobierz_testy(src) %>%
+         filter(rok == rok) %>%
+         select(id_testu, rodzaj_egzaminu, czesc_egzaminu, rok) %>%
+         rename(id = id_testu) %>%
+         collect() %>%
+         as.data.frame()
+  typy_testow <<- rbind(typy_testow, nowe)
+}
+
+przeladuj_kryteria <- function(rok) {
+  src = polacz()
+  kryt = pobierz_kryteria_oceny(src) %>%
+         select(kryterium, numer_pytania, id_wiazki, numer_kryterium, id_testu, l_punktow) %>%
+         rename(id = kryterium, max_punktow = l_punktow) %>%
+         collect() %>%
+         as.data.frame()
+  kryteria <<- kryt
+}
+
+zaladuj_male_dane <- function () {
+  przeladuj_regiony()
+  przeladuj_kryteria()
+}
+
+zaladuj_male_dane_rok <- function(rok) {
+  zaladuj_nowe_szkoly(rok)
+  zaladuj_nowe_testy(rok)
+}
+
+zaladuj_nowe_wyniki_szkol <- function(rok) {
+  src = polacz()
+
+  testy = pobierz_testy(src) %>%
+          filter(rok == rok) %>%
+          select(id_testu)
+  dane = pobierz_odpowiedzi() %>%
+        join(testy) %>%
+        filter(grepl("k", kryterium)) %>% # odfiltruj tylko prawdziwe kryteria
+        select(id_szkoly, id_testu, id_obserwacji, kryterium, ocena) %>%
+        collect()
+
+  wyniki = dane %>%
+           group_by(id_szkoly, id_testu, kryterium) %>%
+           summarise(sredni_wynik = avg(ocena)) %>%
+           rename(id = id_szkoly, id_kryterium = kryterium) %>%
+           as.data.frame()
+  wyniki_szkol <<- rbind(wyniki_szkol, wyniki)
+
+  uczszk = pobierz_dane_uczniowie_testy(src) %>%
+           join(testy) %>%
+           select(id_obserwacji, id_szkoly) %>%
+           unique(id_obserwacji, id_szkoly) %>%
+           collect()
+
+  oceny = dane %>%
+          zsumuj_punkty() %>%
+          select(id_obserwacji, id_testu, wynik) %>%
+          join(uczszk) %>%
+          group_by(id_testu, id_szkoly) %>%
+          summarise(sredni_wynik = avg(wynik)) %>%
+          rename(id = id_szkoly) %>%
+          as.data.frame()
+
+  oceny_szkol <<- rbind(oceny_szkol, oceny)
+}
+
+zaladuj_duze_dane_rok <- function(rok) {
+  zaladuj_nowe_wyniki_szkol(rok)
 }
