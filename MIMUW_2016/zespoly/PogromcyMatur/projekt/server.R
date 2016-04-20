10249fb6eeef6d305714cf92b32dc23f9c4211b2
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2016-04-20 13:45:00 +0200

    Prototyp funkcji rysujacej pozadany wykres
    Brakuje w niej na razie wyboru regionow i typow szkol, wykres
    przedstawia tez jedynie kryteria, lecz to dobry punkt wyjscia do
    dalszego rozwoju.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- MIMUW_2016/zespoly/PogromcyMatur/projekt/server.R
+++ MIMUW_2016/zespoly/PogromcyMatur/projekt/server.R
@@ -9,6 +9,54 @@ library(shiny)
 library(dplyr)
 library(ZPD)
 library(ggplot2)
+library(scales)
+
+load("test/bigData.RData")
+load("test/smallData.RData")
+
+# Generuje wykres wyników szkół po kryteriach z testu "id_testu_wyniki" porządkowanych
+# po "id_testu_ocena". Kwantyle to lista kwantyli (dwuelementowych list początek, koniec).
+# Przykładowo pojedynczy kwantyl to (0.5, 0.7).
+getplot <- function(id_testu_wyniki, id_testu_ocena, kwantyle) {
+
+  # Wyciągnij najpierw dane o czkołach, których dotyczą te testy
+  dane = oceny_szkol %>%
+         filter(id_testu == id_testu_ocena) %>%
+         arrange(sredni_wynik) %>%
+         select(-id_testu)
+  szkoly = dane %>% select(id)
+  wyniki = wyniki_szkol %>%
+           filter(id_testu == id_testu_wyniki) %>%
+           inner_join(szkoly) %>%
+           select(-id_testu)
+  dane_wykresu = NULL
+
+  # Dla każdego kwantyla wyciągnij i uśrednij dane z wyniki_szkol
+  for (i in 1:length(kwantyle)) {
+    kwantyl = dane %>%
+              filter(sredni_wynik >= quantile(dane$sredni_wynik, kwantyle[[i]][[1]]),
+                     sredni_wynik <= quantile(dane$sredni_wynik, kwantyle[[i]][[2]])) %>%
+              select(id)
+    wynikk = wyniki %>%
+             inner_join(kwantyl) %>%
+             group_by(id_kryterium) %>%
+             summarise(suma_wynik = mean(sredni_wynik)) %>%
+             select(id_kryterium, suma_wynik)
+    wynikk$kwantyl = paste(as.character(kwantyle[[i]][[1]]), ":", as.character(kwantyle[[i]][[2]]))
+    dane_wykresu <- rbind(dane_wykresu, wynikk)
+  }
+
+  #Przeskaluj dane na procenty
+  dane_z_kryt = dane_wykresu %>%
+                 inner_join(kryteria, by = c("id_kryterium" = "id"))
+  dane_wykresu$suma_wynik = dane_z_kryt$suma_wynik / dane_z_kryt$max_punktow
+
+  #Wykres
+  plot = ggplot(dane_wykresu, aes(x = id_kryterium, y = suma_wynik, fill = factor(kwantyl))) + geom_bar(stat="identity", position = "dodge") +
+  scale_fill_discrete(name = "Kwantyl") + ylab("Średni wynik (%)") + xlab("Id kryterium") +
+  scale_y_continuous(labels = scales::percent)
+  return(plot)
+}
 
 shinyServer(function(input, output) {
   tmp = as.list(1:7)
@@ -27,6 +75,3 @@ shinyServer(function(input, output) {
   })
   
 })
-
-load("test/bigData.RData")
-load("test/smallData.RData")
