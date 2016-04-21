68e8d10b6ceb8076b374767db7ff41e4d328cc46
unknown <bb332063@BLUE13.ad.mimuw.edu.pl>
2016-04-21 16:28:45 +0200

    Aplikacja shiny: kolejny etap

--- MIMUW_2016/zespoly/teamRocket/data/app/server.R
+++ MIMUW_2016/zespoly/teamRocket/data/app/server.R
@@ -8,50 +8,106 @@ library(rCharts)
 
 # ~~~~~~~~~~~~~~~~~~~~~~UWAGA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Zmiana przelicznika. Zamiast punktow trzeba liczyc procenty - na razie tylko dla mimuw
-mimuw_classify<-filter(data.wawa, p_mat>15 & p_pl>15 & r_mat>0)
+mimuw_classify <- filter(data.wawa, p_mat>15 & p_pl>15 & r_mat>0)
 mimuw_classify <- mimuw_classify %>% rowwise() %>% mutate(wynik = 
-                                                            0.1 * max(0.6*p_pl*10/7, r_pl*10/4, na.rm = TRUE) + 
-                                                            0.1 * max(0.6*p_mat*2, r_mat*2, na.rm = TRUE) +
-                                                            0.1 * max(0.6*p_ang*2, r_ang*2, na.rm = TRUE) + 
-                                                            0.5 * max(r_inf*2, r_mat*2, na.rm = TRUE) + 
-                                                            0.2 * max(r_mat*2, r_pl*10/4, r_ang*2, r_fiz*10/6, r_bio*10/6, r_chem*10/6, r_inf*2, na.rm = TRUE)
+                                                            0.1 * max(0, 0.6*p_pl*10/7, r_pl*10/4, na.rm = TRUE) + 
+                                                            0.1 * max(0, 0.6*p_mat*2, r_mat*2, na.rm = TRUE) +
+                                                            0.1 * max(0, 0.6*p_ang*2, r_ang*2, na.rm = TRUE) + 
+                                                            0.5 * max(0, r_inf*2, r_mat*2, na.rm = TRUE) + 
+                                                            0.2 * max(0, r_mat*2, r_pl*10/4, r_ang*2, r_fiz*10/6, r_bio*10/6, r_chem*10/6, r_inf*2, na.rm = TRUE)
 )
 mini_classify <- filter(data.wawa, p_mat>15 & p_pl>15 & r_mat>0 & (r_bio > 0 | r_fiz > 0 | r_chem>0 | r_inf > 0))
 mini_classify <- mini_classify %>% rowwise() %>% mutate(wynik = 
-                                                          max(0.5*p_mat*2, r_mat*2, na.rm = TRUE)*4/9 +
-                                                          0.25 * max(0.5*p_ang*2,r_ang*2, na.rm = TRUE)/9 + 
-                                                          max(r_fiz, 0.5*r_bio*10/6, r_fiz*10/6, 0.75*r_chem*10/6, r_inf*2, na.rm = TRUE)*4/9
+                                                          max(0, 0.5*p_mat*2, r_mat*2, na.rm = TRUE)*4/9 +
+                                                          0.25 * max(0, 0.5*p_ang*2,r_ang*2, na.rm = TRUE)/9 + 
+                                                          max(0, r_fiz*10/6, 0.5*r_bio*10/6, r_fiz*10/6, 0.75*r_chem*10/6, r_inf*2, na.rm = TRUE)*4/9
 )
 uj_classify <- filter(data.wawa, p_mat>15 & p_pl>15 & sum(c(r_inf>0,r_mat>0, r_bio>0,r_fiz>0,r_chem>0), na.rm = TRUE)>1)
-uj_classify <- mini_classify %>% rowwise() %>% mutate(wynik = 
-                                                        max(max(0.5*p_mat*2, r_mat*2, na.rm = TRUE), 
-                                                            max(0.5*p_inf*2, r_inf*2, na.rm = TRUE),
-                                                            max(0.5*p_bio*10/6, r_bio*10/6, na.rm = TRUE),
-                                                            max(0.5*p_fiz*10/6, r_fiz*10/6, na.rm = TRUE),
-                                                            max(0.5*p_chem*10/6, r_chem*10/6, na.rm = TRUE),
+uj_classify <- uj_classify %>% rowwise() %>% mutate(wynik = 
+                                                        max(max(0, 0.5*p_mat*2, r_mat*2, na.rm = TRUE), 
+                                                            max(0, 0.5*p_inf*2, r_inf*2, na.rm = TRUE),
+                                                            max(0, 0.5*p_bio*10/6, r_bio*10/6, na.rm = TRUE),
+                                                            max(0, 0.5*p_fiz*10/6, r_fiz*10/6, na.rm = TRUE),
+                                                            max(0, 0.5*p_chem*10/6, r_chem*10/6, na.rm = TRUE),
                                                             na.rm = TRUE)/2 + 
                                                         
                                                         sort(
                                                           c(
-                                                            max(0.5*p_mat*2, r_mat*2, na.rm = TRUE), 
-                                                            max(0.5*p_inf*2, r_inf*2, na.rm = TRUE),
-                                                            max(0.5*p_bio*10/6, r_bio*10/6, na.rm = TRUE),
-                                                            max(0.5*p_fiz*10/6, r_fiz*10/6, na.rm = TRUE),
-                                                            max(0.5*p_chem*10/6, r_chem*10/6, na.rm = TRUE)
+                                                            max(0, 0.5*p_mat*2, r_mat*2, na.rm = TRUE), 
+                                                            max(0, 0.5*p_inf*2, r_inf*2, na.rm = TRUE),
+                                                            max(0, 0.5*p_bio*10/6, r_bio*10/6, na.rm = TRUE),
+                                                            max(0, 0.5*p_fiz*10/6, r_fiz*10/6, na.rm = TRUE),
+                                                            max(0, 0.5*p_chem*10/6, r_chem*10/6, na.rm = TRUE)
                                                           ),
                                                           ,partial=4)[4]/2
 )
 
-uksford_classify<-filter(data.wawa, p_mat>15 & p_pl>15 & r_mat>0)
+uksford_classify <- filter(data.wawa, p_mat>15 & p_pl>15 & r_mat>0)
 uksford_classify <- uksford_classify %>% rowwise() %>% mutate(wynik = 
-                                                                max(0.4*p_mat*2, 0.8*r_mat*2, na.rm = TRUE) +
-                                                                max(0.1*p_ang*2, 0.2*r_ang*2, na.rm = TRUE)
+                                                                max(0, 0.4*p_mat*2, 0.8*r_mat*2, na.rm = TRUE) +
+                                                                max(0, 0.1*p_ang*2, 0.2*r_ang*2, na.rm = TRUE)
 )
 
 
 shinyServer(function(input, output) {
-  output$wykresSlupkowy = renderPlot({
+  output$wykresSlupkowy = renderPrint({
+    if (input$wybranaUczelnia == 'UW') {
+      classify <- mimuw_classify
+      if (input$wybranyKierunek == 'Matematyka')
+        prog <- 72
+      else if (input$wybranyKierunek == 'Informatyka')
+        prog <- 85
+      else
+        prog <- 0
+    } else if (input$wybranaUczelnia == 'PW') {
+      classify <- mini_classify
+      if (input$wybranyKierunek == 'Matematyka')
+        prog <- 70
+      else if (input$wybranyKierunek == 'Informatyka')
+        prog <- 80
+      else
+        prog <- 0
+    } else if (input$wybranaUczelnia == 'UJ') {
+      classify <- uj_classify
+      if (input$wybranyKierunek == 'Matematyka')
+        prog <- 70
+      else if (input$wybranyKierunek == 'Informatyka')
+        prog <- 80
+      else
+        prog <- 0
+    } else if (input$wybranaUczelnia == 'UKSW') {
+      classify <- uksford_classify
+      if (input$wybranyKierunek == 'Matematyka')
+        prog <- 70
+      else if (input$wybranyKierunek == 'Informatyka')
+        prog <- 80
+      else
+        prog <- 0
+    }
     
+    # Lista 300 najlepszych studentow ogolnie + wynik 300 studenta
+    best_300 <- classify %>% arrange(desc(wynik)) %>% head(300)
+    wynik_300 <- best_300[300,]$wynik
+    
+    # Wyliczanie top 5 szkol
+    top <- classify %>% group_by(id_szkoly) %>%
+      summarise(all_count = n(), top_300_count = sum(wynik >= wynik_300), procent = 100*top_300_count/all_count) %>%
+      arrange(desc(procent)) %>% filter(all_count > 10) %>% head(input$ileSzkol) %>%
+      merge(filter(szkoly, rok==2015), by = "id_szkoly") %>% merge(classify, by = "id_szkoly")
+    
+    # Zamiana id_szkoly z typu numeric na character
+    top$id_szkoly <- as.character(top$id_szkoly)
+    
+    procent <- top %>% group_by(nazwa_szkoly) %>% summarise(procent = mean(wynik > prog))
+    procent
+#     
+#     slupkowy <- ggplot(procent) +
+#       theme(axis.title.x=element_blank(), axis.text.x=element_blank(),
+#             axis.ticks.x=element_blank(), legend.text=element_text(size=7.6),
+#             legend.position='right') +
+#       geom_bar() +
+#       expand_limits(y=c(15, 100))
+#     slupkowy
   })
   
   output$wykresSrzypcowy = renderPlot({
@@ -94,15 +150,16 @@ shinyServer(function(input, output) {
     wynik_300 <- best_300[300,]$wynik
     
     # Wyliczanie top 5 szkol
-    top5 <- classify %>% group_by(id_szkoly) %>%
+    top <- classify %>% group_by(id_szkoly) %>%
       summarise(all_count = n(), top_300_count = sum(wynik >= wynik_300), procent = 100*top_300_count/all_count) %>%
-      arrange(desc(procent)) %>% filter(all_count > 10) %>% head(5) %>%
+      arrange(desc(procent)) %>% filter(all_count > 10) %>% head(input$ileSzkol) %>%
       merge(filter(szkoly, rok==2015), by = "id_szkoly") %>% merge(classify, by = "id_szkoly")
     
     # Zamiana id_szkoly z typu numeric na character
-    top5$id_szkoly <- as.character(top5$id_szkoly)
+    top$id_szkoly <- as.character(top$id_szkoly)
     
-    skrzypcowy <- ggplot(top5, aes(x = id_szkoly, y = wynik, group=id_szkoly, fill=nazwa_szkoly)) +
+    
+    skrzypcowy <- ggplot(top, aes(x = id_szkoly, y = wynik, group=id_szkoly, fill=nazwa_szkoly)) +
       theme(axis.title.x=element_blank(), axis.text.x=element_blank(),
             axis.ticks.x=element_blank(), legend.text=element_text(size=7.6),
             legend.position='right') +
--- MIMUW_2016/zespoly/teamRocket/data/app/ui.R
+++ MIMUW_2016/zespoly/teamRocket/data/app/ui.R
@@ -2,35 +2,59 @@ library(shiny)
 
 
 shinyUI(fluidPage(
-  titlePanel("Szkoły najlepiej przygotowujące na wybrany kierunek studiów"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "region",
-                  label = "Szkoły: Wybierz region",
-                  choices = c("Warszawa"),
-                  selected = "Warszawa"),
-      selectInput(inputId = "wybranaUczelnia", 
-                  label = "Wybierz uczelnię",
-                  choices = c("UW", "PW", "UJ", "UKSW"),
-                  selected = "UW"),
-      selectInput(inputId = "wybranyKierunek", 
-                  label = "Wybierz kierunek",
-                  choices = c("Matematyka", "Informatyka"),
-                  selected = "Informatyka"),
-      checkboxInput(inputId = "prog",
-                    label = "Czy zaznaczyć próg?",
-                    value = TRUE)
-    ),
-    mainPanel(
-      tabsetPanel(
-        # tabPanel("Ranking",
-        #   plotOutput("wykresSlupkowy"),
-        #   verbatimTextOutput("ranking")
-        # ),
-        tabPanel("Rozkład wyników",
-          plotOutput("wykresSrzypcowy")
+  tabsetPanel(
+    tabPanel(
+      "Najlepsze szkoły",
+      titlePanel("Szkoły najlepiej przygotowujące na wybrany kierunek studiów"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(inputId = "region",
+                      label = "Szkoły: Wybierz region",
+                      choices = c("Warszawa"),
+                      selected = "Warszawa"),
+          selectInput(inputId = "wybranaUczelnia", 
+                      label = "Wybierz uczelnię",
+                      choices = c("UW", "PW", "UJ", "UKSW"),
+                      selected = "UW"),
+          selectInput(inputId = "wybranyKierunek", 
+                      label = "Wybierz kierunek",
+                      choices = c("Matematyka", "Informatyka"),
+                      selected = "Informatyka"),
+          sliderInput("ileSzkol", label = "Ile szkół?", min = 2, max = 10, value = 5),
+          checkboxInput(inputId = "prog",
+                        label = "Czy zaznaczyć próg?",
+                        value = TRUE)
+        ),
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Ranking",
+                     verbatimTextOutput("wykresSlupkowy"),
+              verbatimTextOutput("ranking")
+            ),
+            tabPanel("Rozkład wyników",
+              plotOutput("wykresSrzypcowy")
+            )
+          )
         )
       )
+    ),
+    tabPanel(
+      "Profil szkoły",
+      titlePanel("Profil wybranych szkół"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(inputId = "region2",
+                      label = "Szkoły: Wybierz region",
+                      choices = c("Warszawa"),
+                      selected = "Warszawa"),
+          checkboxGroupInput("przedmioty", label = "Szkoły", choices = names(data.wawa)[2:15])
+        ),
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Rankingg"
+            )
+          )
+        )
     )
   )
-))
+)))
