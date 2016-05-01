2057b4eeeca5c840117b164ddedcf646d5323294
SzymonDz <szymon.dziewiatkowski@gmail.com>
2016-05-01 23:28:56 +0100

    Added plot

--- MIMUW_2016/zespoly/marakuja/course-correlation-app/example.R
+++ MIMUW_2016/zespoly/marakuja/course-correlation-app/example.R
@@ -5,6 +5,8 @@ courseB = '1000-214bSIK'
 
 count_A_by_mark_B_all(courseB)
 count_A_by_mark_B_failed(courseA, courseB)
-count_A_by_mark_B_passed(courseA, courseB)
+passed <- count_A_by_mark_B_passed(courseA, courseB)
 count_A_by_mark_B_attending(courseA, courseB)
 count_A_by_mark_B_not_attending(courseA, courseB)
+
+ggplot(df_for_plot(passed), aes(x = ocena_przedmiot_B, y = liczba_studentow)) + geom_step()
