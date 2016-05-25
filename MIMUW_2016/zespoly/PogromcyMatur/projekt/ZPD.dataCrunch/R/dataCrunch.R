591f1fdeeb0fbce8816206e1ce9f9a75ff809037
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2016-05-25 23:23:28 +0200

    Package data crunching functions into a module
    Create a class that holds all of the related tables together.
    Pass it around in functions that add all the new data to it
    instead of modifying global namespace. Add some docs.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

---
+++ MIMUW_2016/zespoly/PogromcyMatur/projekt/ZPD.dataCrunch/R/dataCrunch.R
@@ -0,0 +1,307 @@
+.przeladuj_regiony <- function(ctxt) {
+  src = polacz()
+  reg = pobierz_szkoly(src) %>%
+    select(gmina_szkoly, powiat_szkoly, wojewodztwo_szkoly) %>%
+    distinct() %>%
+    rename(gmina = gmina_szkoly, powiat = powiat_szkoly, wojewodztwo = wojewodztwo_szkoly) %>%
+    collect() %>%
+    as.data.frame()
+  ctxt@regiony <- reg
+  return(ctxt)
+}
+
+.zaladuj_nowe_szkoly <- function(ctxt, p_rok) {
+  src = polacz()
+  nowe = pobierz_szkoly(src) %>%
+    filter(rok == p_rok) %>%
+    select(id_szkoly, gmina_szkoly, powiat_szkoly, wojewodztwo_szkoly, nazwa_szkoly, typ_szkoly, rok) %>%
+    rename(id = id_szkoly, gmina = gmina_szkoly, powiat = powiat_szkoly, wojewodztwo = wojewodztwo_szkoly,
+           nazwa = nazwa_szkoly, typ = typ_szkoly) %>%
+    collect() %>%
+    as.data.frame()
+  ctxt@szkoly <- rbind(ctxt@szkoly, nowe)
+  ctxt@szkoly <- ctxt@szkoly %>% distinct()
+  return(ctxt)
+}
+
+.zaladuj_nowe_testy <- function(ctxt, p_rok) {
+  src = polacz()
+  nowe = pobierz_testy(src) %>%
+    filter(rok == p_rok, czy_egzamin == TRUE, dane_ewd == TRUE) %>%
+    filter(rodzaj_egzaminu != "matura poprawkowa") %>% # TODO - jakos uwzgledniac
+    select(id_testu, rodzaj_egzaminu, czesc_egzaminu, rok) %>%
+    rename(id = id_testu) %>%
+    collect() %>%
+    as.data.frame()
+  ctxt@typy_testow <- rbind(ctxt@typy_testow, nowe)
+  ctxt@typy_testow <- ctxt@typy_testow %>% unique()
+  return(ctxt)
+}
+
+.przeladuj_kryteria <- function(ctxt) {
+  src = polacz()
+  kryt = pobierz_kryteria_oceny(src) %>%
+    select(kryterium, l_punktow,
+           numer_pytania, numer_kryterium, id_pytania, id_wiazki, tresc_pytania, tresc_wiazki) %>%
+    rename(id = kryterium, max_punktow = l_punktow) %>%
+    collect() %>%
+    distinct() %>%
+    as.data.frame()
+  ctxt@kryteria <- kryt
+  return(ctxt)
+}
+
+.zaladuj_normy <- function(ctxt) {
+  src = polacz()
+  skalowania <- pobierz_skale(src) %>%
+    filter(rodzaj_skali == 'zrównywanie', posiada_normy) %>%
+    select(id_skali, id_testu, skalowanie, opis_skalowania)
+  
+  ctxt@normy <- pobierz_normy(src) %>%
+    inner_join(skalowania) %>%
+    select(id_testu, wartosc_zr, wartosc) %>%
+    collect()
+  return(ctxt)
+}
+
+.zaladuj_uczniow <- function(ctxt) {
+  src = polacz()
+  ctxt@uczniowie <- pobierz_uczniow(src) %>%
+    select(id_obserwacji, plec) %>%
+    as.data.frame()
+  return(ctxt)
+}
+
+#' Pobiera dane pomocnicze niezależne od roku.
+#' 
+#' Część przetwarzanych danych pełni funkcję pomocniczą w przetwarzaniu
+#' wyników egzaminów. Ta funkcja wczytuje je.
+#' 
+#' Dane pobierane w tej funkcji mogą zmieniać się co roku. Wywołanie jej
+#' zastąpi poprzednie dane w dazie danych - aby zaktualizować dane, wystarczy
+#' więc wywołać tę funkcję raz.
+#' @param ctxt Obiekt klasy DataCrunch, w którym mamy umieścić dane.
+#' @return Obiekt DataCrunch z umieszczonymi nowymi danymi.
+#' @export
+zaladuj_male_dane <- function (ctxt) {
+  ctxt = ctxt %>%
+    .przeladuj_regiony() %>%
+    .przeladuj_kryteria() %>%
+    .zaladuj_normy() %>%
+    .zaladuj_ostatnie_przystapienia() %>%
+    .zaladuj_uczniow()
+  return(ctxt)
+}
+
+#' Pobiera dane pomocnicze zależne od roku.
+#' 
+#' Część przetwarzanych danych pełni funkcję pomocniczą w przetwarzaniu
+#' wyników egzaminów. Ta funkcja wczytuje je.
+#' 
+#' Dane pobierane w tej funkcji nie zmieniają się, lecz co roku dodawane są
+#' do nich nowe dane. Aby pobrać dane z danego roku, wywołaj funkcję z odpowiednim
+#' parametrem.
+#' 
+#' @param rok Rok, z którego chcemy pobrać dane pomocnicze.
+#' @param ctxt Obiekt klasy DataCrunch, w którym mamy umieścić dane.
+#' @return Obiekt DataCrunch z umieszczonymi nowymi danymi.
+#' @export
+zaladuj_male_dane_rok <- function(ctxt, rok) {
+  ctxt = ctxt %>%
+    .zaladuj_nowe_szkoly(rok) %>%
+    .zaladuj_nowe_testy(rok)
+  return(ctxt)
+}
+
+.zaladuj_ostatnie_przystapienia <- function(ctxt) {
+  # Nie ma odczuwalnej roznicy miedzy sciaganiem calych danych i robieniem selecta, wiec sciagamy cale dane
+  src = polacz()
+  
+  przystapienia = filtruj_przystapienia(src, FALSE, 'matura', NULL, TRUE) %>% 
+    select(id_obserwacji, rodzaj_egzaminu, rok) %>% collect()
+  ctxt@ostatnie_przystapienia <- przystapienia
+  
+  przystapienia = filtruj_przystapienia(src, FALSE, 'egzamin gimnazjalny', NULL, TRUE) %>%
+    select(id_obserwacji, rodzaj_egzaminu, rok) %>% collect()
+  ctxt@ostatnie_przystapienia <- ctxt@ostatnie_przystapienia %>% rbind(przystapienia)
+  
+  przystapienia = filtruj_przystapienia(src, FALSE, 'sprawdzian', NULL, TRUE) %>%
+    select(id_obserwacji, rodzaj_egzaminu, rok) %>% collect()
+  ctxt@ostatnie_przystapienia <- ctxt@ostatnie_przystapienia %>% rbind(przystapienia)
+  
+  return(ctxt)
+}
+
+.dodaj_sumaryczne_oceny_uczniow <- function(ctxt, src, dane, row) {
+  # Sumaryczne wyniki uczniow, zrownane
+  sumy = rowSums(dane %>% select(starts_with("k_")))
+  nowe_oceny_uczniow <- dane %>%
+    mutate(wynik = sumy) %>%
+    select(id_obserwacji, id_testu, id_szkoly, wynik) %>%
+    inner_join(ctxt@normy, by = c("id_testu" = "id_testu", "wynik" = "wartosc")) %>%
+    select(-wynik) %>%
+    rename(wynik=wartosc_zr) %>%
+    mutate(rodzaj_egzaminu = row$rodzaj_egzaminu, czesc_egzaminu = row$czesc_egzaminu, rok = row$rok)
+  
+  ctxt@oceny_uczniow <- ctxt@oceny_uczniow %>% rbind(as.data.frame(nowe_oceny_uczniow))
+  return(ctxt)
+}
+
+.dodaj_oceny_po_poprzednich <- function(ctxt, src, dane, row) {
+  uczniowie <- dane %>%
+    select(id_obserwacji) %>%
+    distinct()
+  
+  poprzednie_oceny = ctxt@oceny_uczniow %>%
+    inner_join(uczniowie) %>%
+    filter(rodzaj_egzaminu != row$rodzaj_egzaminu) %>% # Rozważaj tylko scisle wczesniejsze egzaminy
+    inner_join(ctxt@ostatnie_przystapienia)
+  
+  poprzednie_egzaminy = poprzednie_oceny %>%
+    select(rodzaj_egzaminu, czesc_egzaminu) %>%
+    distinct()
+  
+  # Posortuj uczniow po poprzednich pisanych egzaminach, posumuj procenty
+  for (j in 1:nrow(poprzednie_egzaminy)) {
+    egz = poprzednie_egzaminy[j,]
+    
+    szer = poprzednie_oceny %>%
+      inner_join(egz) %>%
+      rename(rodzaj_poprzedni=rodzaj_egzaminu, czesc_poprzedni=czesc_egzaminu) %>%
+      select(id_obserwacji, rodzaj_poprzedni, czesc_poprzedni, wynik) %>%
+      inner_join(dane) %>%
+      arrange(wynik) %>%
+      rename(poprzedni_wynik = wynik)
+    
+    szer = szer %>%
+      group_by(poprzedni_wynik, rodzaj_poprzedni, czesc_poprzedni) %>%
+      mutate(liczba = n()) %>%
+      mutate_each(funs(mean(., na.rm = TRUE)),
+                  starts_with("k_")) %>%
+      select(poprzedni_wynik, rodzaj_poprzedni, czesc_poprzedni, starts_with("k_"), liczba) %>%
+      distinct() %>%
+      gather(id_kryterium, wynik, starts_with("k_"), na.rm = TRUE) %>%
+      mutate(rodzaj_egzaminu = row$rodzaj_egzaminu, czesc_egzaminu = row$czesc_egzaminu, rok = row$rok)
+    
+    ctxt@wyniki_po_egz <- ctxt@wyniki_po_egz %>% rbind(as.data.frame(szer))
+  }
+  return(ctxt)
+}
+
+.dodaj_oceny_po_plci <- function(ctxt, dane, row) {
+  nowe = ctxt@uczniowie %>%
+    select(id_obserwacji, plec) %>%
+    filter(!is.na(plec)) %>% # Niektorzy uczniowie nie maja informacji o plci
+    inner_join(dane)
+  nowe = nowe %>%
+    group_by(plec) %>%
+    summarise_each(funs(mean(., na.rm = TRUE)),
+                   starts_with("k_"))
+  nowe = nowe %>%
+    gather(id_kryterium, wynik, starts_with("k_"), na.rm = TRUE)
+  nowe = nowe %>%
+    mutate(rodzaj_egzaminu = row$rodzaj_egzaminu, czesc_egzaminu = row$czesc_egzaminu, rok = row$rok)
+  ctxt@wyniki_po_plci <- ctxt@wyniki_po_plci %>% rbind(as.data.frame(nowe))
+  return(ctxt)
+}
+
+#' Pozwala ręcznie umieścić w danych wyniki danego egzaminu.
+#' 
+#' UWAGA: dane, które w bazie już się znajdują zostaną zignorowane. Jeśli chcesz wprowadzić
+#' je ponownie, użyj funkcji usun_wyniki.
+#' @param ctxt Obiekt klasy DataCrunch, w którym mamy umieścić dane.
+#' @param p_rok Rok, z którego chcemy pobrać egzaminy.
+#' @param t_rodzaj Rodzaj egzaminu, do jakiego chcemy się ograniczyć. Domyślnie
+#' nie filtrujemy po rodzaju egzaminu.
+#' @param t_czesc Część egzaminu, do jakiej chcemy się ograniczyć. Domyślnie
+#' nie filtrujemy po części egzaminu.
+#' @param l_dane Lokalne dane w formie, jak z pobierz_wyniki_egzaminu. Domyślnie dane
+#' pobierane są z serwera ZPD. Jeśli podany, parametry t_rodzaj i t_czesc również powinny
+#' być podane.
+#' 
+#' @return Obiekt danych DataCrunch z dodanymi danymi, jeśli dane zostały poprawnie pobrane
+#' i wprowadzone do bazy. NULL jeśli dane w bazie już są, podano złe argumenty lub nie
+#' znaleziono żądanych testów na serwerze.
+#' @export
+zaladuj_nowe_wyniki <- function(ctxt, p_rok, t_rodzaj = NULL, t_czesc = NULL, l_dane = NULL) {
+  
+  if (!is.null(l_dane) && (is.null(t_rodzaj) || is.null(t_czesc)))
+    return(NULL)
+  
+  src = polacz()
+  
+  #Testy z podanego roku
+  testy = pobierz_testy(src) %>%
+    filter(rok == p_rok, czy_egzamin == TRUE, dane_ewd == TRUE) %>%
+    filter(rodzaj_egzaminu != "matura poprawkowa") %>% # TODO - jakos uwzgledniac
+    select(id_testu, rodzaj_egzaminu, czesc_egzaminu, rok)
+  
+  # Dla kazdego typu egzaminu pobierz dane oddzielnie, by zmiescic sie w pamieci
+  testy_iter = testy %>%
+    collect() %>%
+    select(rodzaj_egzaminu, czesc_egzaminu, rok) %>%
+    distinct()
+  if (!is.null(t_rodzaj)) {
+    testy_iter = testy_iter %>% filter(rodzaj_egzaminu == t_rodzaj)
+  }
+  
+  if (!is.null(t_czesc)) {
+    testy_iter = testy_iter %>% filter(czesc_egzaminu == t_czesc)
+  }
+  
+  # Nie znalezlismy danych testow?
+  if (nrow(testy_iter) == 0)
+    return(NULL)
+  
+  for (i in 1:nrow(testy_iter)) {
+    row = testy_iter[i,]
+    
+    if (nrow(inner_join(row, ctxt@zapisane_testy)) > 0) # Mamy juz ten test
+      next
+    
+    # Wyniki egzaminu
+    if (!is.null(l_dane)) {
+      dane = l_dane %>%
+        collect() %>%
+        select(-rok)
+    } else {
+      dane = pobierz_wyniki_egzaminu(src, row$rodzaj_egzaminu, row$czesc_egzaminu, row$rok, TRUE) %>%
+        collect() %>%
+        select(-rok)
+    }
+    ctxt = ctxt %>%
+      .dodaj_sumaryczne_oceny_uczniow(src, dane, row) %>%
+      .dodaj_oceny_po_poprzednich(src, dane, row) %>%
+      .dodaj_oceny_po_plci(dane, row)
+    ctxt@zapisane_testy <- ctxt@zapisane_testy %>% rbind(as.data.frame(row))
+  }
+  return(ctxt)
+}
+
+#' Usuwa dane z bazy z danego roku.
+#' @param p_rok Rok, który chcemy usunąć.
+#' @param ctxt Obiekt klasy DataCrunch, z którego mamy usunąć dane.
+#' @return Obiekt DataCrunch z usuniętymi danymi.
+#' @export
+usun_wyniki <- function(ctxt, p_rok)
+{
+  ctxt@wyniki_po_egz <- ctxt@wyniki_po_egz %>% filter(rok != p_rok)
+  ctxt@wyniki_po_plci <- ctxt@wyniki_po_plci %>% filter(rok != p_rok)
+  ctxt@oceny_uczniow <- ctxt@oceny_uczniow %>% filter(rok != p_rok)
+  ctxt@zapisane_testy <- ctxt@zapisane_testy %>% filter(rok != p_rok)
+  return(ctxt)
+}
+
+#' Pobiera dane do wykresów zależne od roku.
+#' 
+#' Dane pobierane w tej funkcji nie zmieniają się, lecz co roku dodawane są
+#' do nich nowe dane. Aby pobrać dane z danego roku, wywołaj funkcję z odpowiednim
+#' parametrem.
+#' 
+#' @param rok Rok, z którego chcemy pobrać dane.
+#' @param ctxt Obiekt klasy DataCrunch, w którym mamy umieścić dane.
+#' @return Obiekt DataCrunch z umieszczonymi nowymi danymi.
+#' @export
+zaladuj_duze_dane_rok <- function(ctxt, rok) {
+  return(.zaladuj_nowe_wyniki(ctxt, rok))
+}
---
+++ MIMUW_2016/zespoly/PogromcyMatur/projekt/ZPD.dataCrunch/R/schema.R
@@ -0,0 +1,115 @@
+DataCrunch <- setClass(
+  "DataCrunch",
+  
+  slots = c(
+    typy_testow = "data.frame",
+    zapisane_testy = "data.frame",
+    kryteria = "data.frame",
+    normy = "data.frame",
+    wyniki_po_egz = "data.frame",
+    wyniki_po_plci = "data.frame",
+    oceny_uczniow = "data.frame",
+    ostatnie_przystapienia = "data.frame",
+    uczniowie = "data.frame"
+  ),
+  
+  prototype = list(
+    typy_testow = data.frame(),
+    zapisane_testy = data.frame(),
+    kryteria = data.frame(),
+    normy = data.frame(),
+    wyniki_po_egz = data.frame(),
+    wyniki_po_plci = data.frame(),
+    oceny_uczniow = data.frame(),
+    ostatnie_przystapienia = data.frame(),
+    uczniowie = data.frame()
+  )
+)
+
+#' Tworzy nową grupę pustych danych.
+#' @export
+puste_dane <- function() {
+
+  typy_testow <- data.frame()
+  typy_testow$id <- integer(0)
+  typy_testow$rodzaj_egzaminu <- character(0)
+  typy_testow$czesc_egzaminu <- character(0)
+  typy_testow$rok <- integer(0)
+  typy_testow$arkusz <- character(0)
+  
+  zapisane_testy <- data.frame()
+  zapisane_testy$rok <- integer(0)
+  zapisane_testy$rodzaj_egzaminu <- character(0)
+  zapisane_testy$czesc_egzaminu <- character(0)
+
+  kryteria <- data.frame()
+  kryteria$id <- character(0)
+  kryteria$numer_pytania <- character(0)
+  kryteria$id_pytania <- character(0)
+  kryteria$id_wiazki <- character(0)
+  kryteria$tresc_pytania <- character(0)
+  kryteria$tresc_wiazki <- character(0)
+  kryteria$numer_kryterium <- character(0)
+  kryteria$max_punktow <- integer(0)
+
+  wyniki_po_egz <- data.frame()
+  wyniki_po_egz$rok <- numeric(0)
+  wyniki_po_egz$poprzedni_wynik <- numeric(0)
+  wyniki_po_egz$liczba <- integer(0)
+  wyniki_po_egz$rodzaj_egzaminu <- character(0)
+  wyniki_po_egz$czesc_egzaminu <- character(0)
+  wyniki_po_egz$rodzaj_poprzedni <- character(0)
+  wyniki_po_egz$czesc_poprzedni <- character(0)
+  wyniki_po_egz$id_kryterium <- numeric(0)
+  wyniki_po_egz$wynik <- numeric(0)
+
+  wyniki_po_plci <- data.frame()
+  wyniki_po_plci$rok <- numeric(0)
+  wyniki_po_plci$rodzaj_egzaminu <- character(0)
+  wyniki_po_plci$czesc_egzaminu <- character(0)
+  wyniki_po_plci$plec <- character(0)
+  wyniki_po_plci$id_kryterium <- numeric(0)
+  wyniki_po_plci$wynik <- numeric(0)
+
+  # Tabele pomocnicze
+  oceny_uczniow <- data.frame()
+  oceny_uczniow$id_testu <- integer(0)
+  oceny_uczniow$id_szkoly <- integer(0)
+  oceny_uczniow$rodzaj_egzaminu <- character(0)
+  oceny_uczniow$czesc_egzaminu <- character(0)
+  oceny_uczniow$id_obserwacji <- integer(0)
+  oceny_uczniow$wynik <- numeric(0)
+
+  ostatnie_przystapienia <- data.frame()
+  ostatnie_przystapienia$id_testu <- integer(0)
+  ostatnie_przystapienia$rodzaj_egzaminu <- character(0)
+  ostatnie_przystapienia$czesc_egzaminu <- character(0)
+  ostatnie_przystapienia$rok <- integer(0)
+  
+  normy <- data.frame()
+  normy$id_testu <- integer(0)
+  normy$wartosc_zr <- numeric(0)
+  normy$wartosc <- numeric(0)
+  
+  ostatnie_przystapienia <- data.frame()
+  ostatnie_przystapienia$id_obserwacji <- integer(0)
+  ostatnie_przystapienia$rodzaj_egzaminu <- character(0)
+  ostatnie_przystapienia$rok <- integer(0)
+  
+  uczniowie <- data.frame()
+  uczniowie$id_obserwacji <- integer(0)
+  uczniowie$plec <- character(0)
+  
+  c = DataCrunch()
+  c@typy_testow= typy_testow
+  c@zapisane_testy = zapisane_testy
+  c@kryteria = kryteria
+  c@normy = normy
+  c@wyniki_po_egz = wyniki_po_egz
+  c@wyniki_po_plci = wyniki_po_plci
+  c@oceny_uczniow = oceny_uczniow
+  c@ostatnie_przystapienia = ostatnie_przystapienia
+  c@uczniowie = uczniowie
+  
+  return(c)
+}
