6d246cf2bd5e8fb987d8bad5235b7c74baaea13a
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-05-30 13:55:09 +0200

    Dodanie zalążka funkcji do pobierania dodatkowych zestawów danych
    Przeniesienie jednej funkcji w addColumns do wnętrza innej
    Dodanie sugerowanych paczek: ZPD i devtools

--- MIMUW_2016/zespoly/teamRocket/package/R/addColumns.R
+++ MIMUW_2016/zespoly/teamRocket/package/R/addColumns.R
@@ -1,13 +1,3 @@
-# library(dplyr)
-
-#' Add single new column to table
-#' @param table table to which new column should be added
-#' @param nameFunct pair (newColumnName, newColumnValue)
-#' @return table with added new column
-addSingle <- function(table, nameFunct){
-  dplyr::mutate_(table, .dots=setNames(nameFunct[2], nameFunct[1]))
-}
-
 #' Add list of new columns to a table
 #' @param table table to which new column should be added
 #' @param funcList list containing pairs of new column name and new column lazy-evaluable value
@@ -20,13 +10,8 @@ addSingle <- function(table, nameFunct){
 #' )
 #' y <- addList(x, l)
 addList <- function(table, funcList){
+  addSingle <- function(table, nameFunct){
+    dplyr::mutate_(table, .dots=setNames(nameFunct[2], nameFunct[1]))
+  }
   Reduce(addSingle, funcList, table)
 }
-
-
-#' example use:
-#' load("ZPD_iter3.dat")
-#' l = list(
-#'  c("suma_mat", "p_mat + r_mat"),
-#'  c("srednia_mat", "mean(c(p_mat, r_mat), na.rm=TRUE)")
-#' dd = addList(data.all.pl, l)
