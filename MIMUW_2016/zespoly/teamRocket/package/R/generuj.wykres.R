3606b995520da2ff954c460aa7377b10142f0daf
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-06-01 00:58:03 +0200

    Fine-tuning funkcji generuj.wykres
    Dodanie pakietu zoo jako dependency

--- MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.R
+++ MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.R
@@ -1,28 +1,36 @@
 # Tworzenie nowej tabeli na zależność wynik procentowy uzyskany przez procent uczniow.
 generuj_tabele_pp <- function(dane, nazwa_kolumny) {
-  x <- hist(dane[[nazwa_kolumny]], plot=FALSE)
+  x <- graphics::hist(dane[[nazwa_kolumny]], breaks = seq(0,102, 2), plot=FALSE)
   d <- data.frame(wynik_procentowy = x$mids, procent_osob = 100 * x$counts / nrow(dane))
   return(d)
 }
 
-# Generuj dowolny wykres.
-generuj_wykres <- function(dane, nazwa_kolumny, szkola_id, opis) {
-  dane_szkola <- dplyr::filter(dane, id_szkoly==szkola_id)
-  dane_gmina <- dplyr::filter(dane, gmina_szkoly==dane_szkola[1,]$gmina_szkoly)
+#' Generuj wykresy porównawcze dla konkretnego współczynnika
+#' @param dane tabela z której generować wykres
+#' @param nazwa.kolumny nazwa kolumny 
+generuj.wykres <- function(dane, nazwa.kolumny, szkola_id, opis) {
+  dane_szkola <- dplyr::filter_(dane, ~(id_szkoly==szkola_id))
+  dane_gmina <- dplyr::filter_(dane, ~(gmina_szkoly==dplyr::first(dane_szkola$gmina_szkoly)))
   
   # Tworzenie nowej tabeli na zależność wynik procentowy uzyskany przez procent uczniow.
-  dane_procentowe <- generuj_tabele_pp(dane, nazwa_kolumny)
-  dane_procentowe_szkola <- generuj_tabele_pp(dane_szkola, nazwa_kolumny)
-  dane_procentowe_gmina <- generuj_tabele_pp(dane_gmina, nazwa_kolumny)
+  # Zawartość tabel wygładzana
+  dane_procentowe <- as.data.frame(zoo::rollmean(generuj_tabele_pp(dane, nazwa.kolumny), 3))
+  dane_procentowe_szkola <- as.data.frame(zoo::rollmean(generuj_tabele_pp(dane_szkola, nazwa.kolumny), 3))
+  dane_procentowe_gmina <- as.data.frame(zoo::rollmean(generuj_tabele_pp(dane_gmina, nazwa.kolumny), 3))
   
-  wykres <- ggplot() +
-            geom_smooth(se=FALSE, data=dane_procentowe_szkola, aes(x=wynik_procentowy, y=procent_osob), colour="red", size=1.5) +
-            geom_smooth(se=FALSE, data=dane_procentowe_gmina, aes(x=wynik_procentowy, y=procent_osob), colour="#9F9F8F", size=1.2) +
-            geom_smooth(se=FALSE, data=dane_procentowe, aes(x=wynik_procentowy, y=procent_osob), colour="#2F87EB", size=1.2) +
-            labs(title = opis) + xlab('Wynik procentowy') + ylab('Liczba uczniów z danym wynikiem do ogólnej liczby uczniów') + 
-            theme(legend.position = "bottom",
-                  panel.background = element_rect(fill = "white"),
-                  panel.grid.major = element_line(colour = "#D1D1C1"))
+  spp = 0.3
+  wykres <- ggplot2::ggplot() +
+    ggplot2::geom_smooth(se=FALSE, data=dane_procentowe, ggplot2::aes(x=wynik_procentowy, y=procent_osob,colour="#2F87EB"), span = spp, size=1.2) +
+    ggplot2::geom_smooth(se=FALSE, data=dane_procentowe_gmina, ggplot2::aes(x=wynik_procentowy, y=procent_osob, colour="#9F9F8F"), span = spp,  size=1.2) +
+    ggplot2::geom_smooth(se=FALSE, data=dane_procentowe_szkola, ggplot2::aes(x=wynik_procentowy, y=procent_osob, colour="red"), span = spp, size=1.5) +
+    ggplot2::labs(title = opis) +
+    ggplot2::xlab('Wynik procentowy') +
+    ggplot2::ylab('Relatywny procent uczniów') + 
+    ggplot2::theme(legend.position = "right",
+                  panel.background = ggplot2::element_rect(fill = "white"),
+                  panel.grid.major = ggplot2::element_line(colour = "#D1D1C1"),
+                  legend.key = ggplot2::element_rect(fill = "white")) +
+    ggplot2::scale_color_identity(name="Legenda", guide = "legend", labels = c("kraj", "gmina", "szkoła"))
             
   return(wykres)
 }
