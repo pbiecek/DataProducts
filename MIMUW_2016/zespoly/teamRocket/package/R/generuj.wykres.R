5498e5e2061941eea9aed0b2c4c0246cca592b00
AdriannaTr <traczyk.adrianna@gmail.com>
2016-05-31 22:10:09 +0200

    Wyglad wykresu

--- MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.R
+++ MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.R
@@ -1,14 +1,14 @@
 # Tworzenie nowej tabeli na zależność wynik procentowy uzyskany przez procent uczniow.
 generuj_tabele_pp <- function(dane, nazwa_kolumny) {
-  x <- hist(dane[[nazwa_kolumny]], breaks=100, plot=FALSE)
-  d <- data_frame(wynik_procentowy = x$mids, procent_osob = 100 * x$counts / nrow(dane))
+  x <- hist(dane[[nazwa_kolumny]], plot=FALSE)
+  d <- data.frame(wynik_procentowy = x$mids, procent_osob = 100 * x$counts / nrow(dane))
   return(d)
 }
 
-# Generuj wykres liniowy.
+# Generuj dowolny wykres.
 generuj_wykres <- function(dane, nazwa_kolumny, szkola_id, opis) {
-  dane_szkola <- filter(dane, id_szkoly==szkola_id)
-  dane_gmina <- filter(dane_licz, gmina_szkoly==dane_szkola[1,]$gmina_szkoly)
+  dane_szkola <- dplyr::filter(dane, id_szkoly==szkola_id)
+  dane_gmina <- dplyr::filter(dane, gmina_szkoly==dane_szkola[1,]$gmina_szkoly)
   
   # Tworzenie nowej tabeli na zależność wynik procentowy uzyskany przez procent uczniow.
   dane_procentowe <- generuj_tabele_pp(dane, nazwa_kolumny)
@@ -16,9 +16,13 @@ generuj_wykres <- function(dane, nazwa_kolumny, szkola_id, opis) {
   dane_procentowe_gmina <- generuj_tabele_pp(dane_gmina, nazwa_kolumny)
   
   wykres <- ggplot() +
-            geom_line(data=dane_procentowe_szkola, aes(x=wynik_procentowy, y=procent_osob), colour="red") +
-            geom_line(data=dane_procentowe_gmina, aes(x=wynik_procentowy, y=procent_osob), colour="#BEE4A9") +
-            geom_line(data=dane_procentowe, aes(x=wynik_procentowy, y=procent_osob), colour="gray")
+            geom_smooth(se=FALSE, data=dane_procentowe_szkola, aes(x=wynik_procentowy, y=procent_osob), colour="red", size=1.5) +
+            geom_smooth(se=FALSE, data=dane_procentowe_gmina, aes(x=wynik_procentowy, y=procent_osob), colour="#9F9F8F", size=1.2) +
+            geom_smooth(se=FALSE, data=dane_procentowe, aes(x=wynik_procentowy, y=procent_osob), colour="#2F87EB", size=1.2) +
+            labs(title = opis) + xlab('Wynik procentowy') + ylab('Liczba uczniów z danym wynikiem do ogólnej liczby uczniów') + 
+            theme(legend.position = "bottom",
+                  panel.background = element_rect(fill = "white"),
+                  panel.grid.major = element_line(colour = "#D1D1C1"))
             
   return(wykres)
 }
