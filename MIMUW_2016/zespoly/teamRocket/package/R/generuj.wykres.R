9c63f111a8ca89ad2cef0ac90e6cf6c1b1a6fc47
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-06-01 21:01:46 +0200

    Poprawienie opisów wyznaczników i usunięcie niepotrzebnego kodu z generuj.wykres

--- MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.R
+++ MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.R
@@ -1,31 +1,13 @@
-# Tworzenie nowej tabeli na zależność wynik procentowy uzyskany przez procent uczniow.
-generuj_tabele_pp <- function(dane, nazwa_kolumny) {
-  x <- graphics::hist(dane[[nazwa_kolumny]], plot=FALSE)
-  d <- data.frame(wynik_procentowy = x$mids, procent_osob = 100 * x$counts / nrow(dane))
-  return(d)
-}
-
 #' Generuj wykresy porównawcze dla konkretnego współczynnika
 #' @param dane tabela z której generować wykres
 #' @param nazwa.kolumny nazwa kolumny 
 #' @export
 generuj.wykres <- function(dane, nazwa.kolumny, szkola_id, opis) {
+  # Odfiltrowywanie danych
   dane_szkola <- dplyr::filter(dane, id_szkoly==szkola_id)
   dane_gmina <- dplyr::filter(dane, gmina_szkoly==dplyr::first(dane_szkola$gmina_szkoly))
   
-  # Tworzenie nowej tabeli na zależność wynik procentowy uzyskany przez procent uczniow.
-  # Zawartość tabel wygładzana
-  #dane_procentowe <- as.data.frame(zoo::rollmean(generuj_tabele_pp(dane, nazwa.kolumny), 3))
-  #dane_procentowe_szkola <- as.data.frame(zoo::rollmean(generuj_tabele_pp(dane_szkola, nazwa.kolumny), 3))
-  #dane_procentowe_gmina <- as.data.frame(zoo::rollmean(generuj_tabele_pp(dane_gmina, nazwa.kolumny), 3))
-  #dane_procentowe <- as.data.frame(generuj_tabele_pp(dane, nazwa.kolumny))
-  #dane_procentowe_szkola <- as.data.frame(generuj_tabele_pp(dane_szkola, nazwa.kolumny))
-  #dane_procentowe_gmina <- as.data.frame(generuj_tabele_pp(dane_gmina, nazwa.kolumny))
-  
   wykres <- ggplot2::ggplot() +
-    #ggplot2::geom_smooth(se=FALSE, data=dane_procentowe, ggplot2::aes(x=wynik_procentowy, y=procent_osob,colour="#2F87EB"), span = 0.8, size=1.2) +
-    #ggplot2::geom_smooth(se=FALSE, data=dane_procentowe_gmina, ggplot2::aes(x=wynik_procentowy, y=procent_osob, colour="#9F9F8F"), span = 0.8,  size=1.2) +
-    #ggplot2::geom_smooth(se=FALSE, data=dane_procentowe_szkola, ggplot2::aes(x=wynik_procentowy, y=procent_osob, colour="red"), span = 0.4, size=1.5) +
     ggplot2::geom_freqpoly(data=dane, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"#9F9F8F"'), size = 2, bins = 10) +
     ggplot2::geom_freqpoly(data=dane_gmina, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"#2F87EB"'), size = 1, bins = 10) +
     ggplot2::geom_freqpoly(data=dane_szkola, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"red"'), bins = 10) +
@@ -36,7 +18,7 @@ generuj.wykres <- function(dane, nazwa.kolumny, szkola_id, opis) {
                   panel.background = ggplot2::element_rect(fill = "white"),
                   panel.grid.major = ggplot2::element_line(colour = "#D1D1C1"),
                   legend.key = ggplot2::element_rect(fill = "white")) +
-    ggplot2::scale_color_identity(name="Legenda", guide = "legend", labels = c("kraj", "gmina", "szkoła"))
+    ggplot2::scale_colour_identity(name="Legenda", guide = "legend", labels = c("#9F9F8F" = "kraj", "#2F87EB" = "gmina", "red" = "szkoła"))
             
   return(wykres)
 }
--- MIMUW_2016/zespoly/teamRocket/package/R/wyznaczniki.R
+++ MIMUW_2016/zespoly/teamRocket/package/R/wyznaczniki.R
@@ -9,32 +9,26 @@ all_podst <- "m_bio_p, m_che_p, m_fiz_p, m_geo_p, m_his_p, m_inf_p, m_ang_p, m_p
 uw.matematyka <- list(skrot = "uw_mat", formula = 
                 paste("pmax(m_pol_p*0.6, m_pol_r, na.rm = TRUE) * 0.1 + pmax(m_mat_p * 0.6 * 0.1, m_mat_r * 0.6, na.rm = TRUE) +",
                 " pmax(m_ang_p * 0.6, m_ang_r, na.rm = TRUE) * 0.1 + pmax(", all_rozsz, ", na.rm = TRUE) * 0.2"),
-                opis = paste("Wynik jaki uczniowie szkoły otrzymaliby podczas rekrutacji na matematykę na MIMUW.",
-                "Nie uwzględnia wyników z innych języków obcych niz angielski")
-                )
+                opis = paste("Wynik jaki uczniowie otrzymaliby podczas rekrutacji na matematykę na MIMUW."))
 
 #' Krotka zawierająca opis funkcji wyliczjącej punkty przy rekrutacji na informatykę UW
 #' @export
 uw.informatyka <- list(skrot = "uw_inf", formula = 
                     paste("pmax(m_pol_p*0.6, m_pol_r, na.rm = TRUE) * 0.1 + pmax(m_mat_p * 0.6, m_mat_r, na.rm = TRUE) * 0.1 + 0.5 * pmax(m_mat_r,m_inf_r, na.rm = TRUE) +",
                           " pmax(m_ang_p * 0.6, m_ang_r, na.rm = TRUE) * 0.1 + pmax(", all_rozsz, ", na.rm = TRUE) * 0.2"),
-                  opis = paste("Wynik jaki uczniowie szkoły otrzymaliby podczas rekrutacji na informatykę na MIMUW.",
-                               "Nie uwzględnia wyników z innych języków obcych niz angielski")
-)
+                  opis = paste("Wynik jaki uczniowie otrzymaliby podczas rekrutacji na informatykę na MIMUW."))
 
 #' Krotka zawierająca opis funkcji wyliczjącej czy osoba zdała maturę
 #' @export
 zdawalnosc <- list(skrot = "zdawalnosc", formula = "ifelse(m_mat_p >= 30 &  m_pol_p >= 30 & m_ang_p >= 30, 1, 0)",
-                   opis = paste("Uczniowie którzy zdali maturę (oznaczeni jako 1) i ci którzy nie zdali (oznaczeni jako 0)",
-                                "Nie uwzględnia wyników z innych języków obcych niz angielski"))
+                   opis = paste("Zdawalność matury [0 = nie zdał, 1 = zdał]"))
 
 #' Krotka zawierająca opis funkcji wyliczjącej punkty przy rekrutacji na dziennikarstwo UW
 #' @export
 uw.dziennikarstwo <- list(skrot = "uw_dzien", formula = paste("pmax(m_pol_p * 0.6, m_pol_r, na.rm = TRUE) * 0.3 + ",
                   "pmax(m_mat_p * 0.6, m_mat_r, na.rm = TRUE) * 0.05 + pmax(m_ang_p * 0.6, m_ang_r, na.rm = TRUE) * 0.2 + ",
                   "pmax(0.6 * pmax(m_his_p, m_wos_p, m_geo_p, m_inf_p, na.rm = TRUE), pmax(m_his_r, m_wos_r, m_geo_r, m_inf_r, na.rm = TRUE), na.rm = TRUE) * 0.45"),
-                  opis = paste("Wynik jaki uczniowie szkoły otrzymaliby podczas rekrutacji na Dziennikarstwo UW.",
-                  "Nie uwzględnia wyników z innych języków obcych niz angielski"))
+                  opis = paste("Wynik jaki uczniowie otrzymaliby podczas rekrutacji na Dziennikarstwo UW."))
 
 #' Krotka zawierająca opis funkcji wyliczjącej punkty przy rekrutacji na PRAWO UW
 #' @export
@@ -42,6 +36,5 @@ uw.prawo <- list(skrot = "uw_prawo", formula = paste("pmax(m_pol_p * 0.6, m_pol_
                   "pmax(m_mat_p * 0.6, m_mat_r, na.rm = TRUE) * 0.05 + pmax(m_ang_p * 0.6, m_ang_r, na.rm = TRUE) * 0.1",
                   " + (pmax(0, m_his_r, na.rm = TRUE) + pmax(0, m_wos_r, na.rm = TRUE) + pmax(0, m_geo_r, na.rm = TRUE) -",
                   "ifelse(!anyNA(c(m_his_r, m_wos_r, m_geo_r)), pmin(m_his_r, m_wos_r, m_geo_r), 0)) * 0.35"),
-              opis = paste("Wynik jaki uczniowie szkoły otrzymaliby podczas rekrutacji na Prawo UW.",
-              "Nie uwzględnia wyników z innych języków obcych niz angielski ani wyników z języka łacińskiego i filozofii"))
+              opis = paste("Wynik jaki uczniowie otrzymaliby podczas rekrutacji na Prawo UW."))
 
