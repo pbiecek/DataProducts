ad7ba262fda1834a43217701e000ead585caf7f6
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-06-04 17:40:52 +0200

    Poprawki w generowaniu wykresów dla porównania szkół
    Poprawione 2 literówki w nazwach zmiennych
    Zmiana etykietek
    Zmiana legendy w wykresie liniowym

--- MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.porownaj.R
+++ MIMUW_2016/zespoly/teamRocket/package/R/generuj.wykres.porownaj.R
@@ -12,18 +12,22 @@
 #' generuj.wykres(matura.2015, "m_mat_r", 24063, 24064, "Matura rozszerzona z matematyki")
 #' }
 #' @export
-generuj.wykres <- function(dane, nazwa.kolumny, szkola1_id, szkola2_id, opis) {
+generuj.wykres.porownaj <- function(dane, nazwa.kolumny, szkola1_id, szkola2_id, opis) {
   # Odfiltrowywanie danych
   dane_szkola1 <- dplyr::filter_(dane, paste0("id_szkoly==", szkola1_id, " & !is.na(", nazwa.kolumny,")"))
   dane_szkola2 <- dplyr::filter_(dane, paste0("id_szkoly==", szkola2_id, " & !is.na(", nazwa.kolumny,")"))
+  # Odzyskaj nazwę szkoły
+  nazwa.szkola.1 <- dplyr::first(dane_szkola1$nazwa_szkoly) %>% strwrap(30) %>% paste(collapse="\n") %>% paste0("\n")
+  nazwa.szkola.2 <- dplyr::first(dane_szkola2$nazwa_szkoly) %>% strwrap(30) %>% paste(collapse="\n") %>% paste0("\n")
+  
   # Czy kolumna typu liczbowego?
   czy.liczbowe <- is.numeric(dane[[nazwa.kolumny]])
   # Różne wykresy zależnie od typu kolumny
   wykres <- if(czy.liczbowe){
     # Dla numerycznych kolumn generuj wykresy "funkcji gęstości"
     ggplot2::ggplot() +
-      ggplot2::geom_freqpoly(data=dane_szkoly1, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"red"'), bins = 10) +
-      ggplot2::geom_freqpoly(data=dane_szkoly2, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"blue"'), bins = 10) +
+      ggplot2::geom_freqpoly(data=dane_szkola1, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"red"'), bins = 10) +
+      ggplot2::geom_freqpoly(data=dane_szkola2, ggplot2::aes_string(x=nazwa.kolumny, "..density..", colour='"blue"'), bins = 10) +
       ggplot2::labs(title = opis) +
       ggplot2::xlab('Wartość wyznacznika') +
       ggplot2::ylab('Częstotliwość') + 
@@ -31,17 +35,17 @@ generuj.wykres <- function(dane, nazwa.kolumny, szkola1_id, szkola2_id, opis) {
                      panel.background = ggplot2::element_rect(fill = "white"),
                      panel.grid.major = ggplot2::element_line(colour = "#D1D1C1"),
                      legend.key = ggplot2::element_rect(fill = "white")) +
-      ggplot2::scale_colour_identity(name="Legenda", guide = "legend", labels = c("red" = szkola1_id, "blue" = szkola2_id))
+      ggplot2::guides(colour = ggplot2::guide_legend(nrow=2, ncol=1))+
+      ggplot2::scale_colour_identity(name="", guide = "legend", labels = c("red" = nazwa.szkola.1, "blue" = nazwa.szkola.2))
   } else {
     # Dla nie-numerycznych kolumn generuj stackowane wykresy słupkowe
     ggplot2::ggplot() +
-      ggplot2::geom_bar(data=dane_szkoly1, ggplot2::aes_string(fill=nazwa.kolumny, y="100*..count../sum(..count..)", x=szkola1_id),position = "stack", na.rm = TRUE)+
-      ggplot2::geom_bar(data=dane_szkoly2, ggplot2::aes_string(fill=nazwa.kolumny, y="100*..count../sum(..count..)", x=szkola2_id),position = "stack", na.rm = TRUE)+
+      ggplot2::geom_bar(data=dane_szkola1, ggplot2::aes_string(fill=nazwa.kolumny, y="100*..count../sum(..count..)", x="nazwa.szkola.1"),position = "stack", na.rm = TRUE)+
+      ggplot2::geom_bar(data=dane_szkola2, ggplot2::aes_string(fill=nazwa.kolumny, y="100*..count../sum(..count..)", x="nazwa.szkola.2"),position = "stack", na.rm = TRUE)+
       ggplot2::labs(title = opis) +
       ggplot2::xlab('') +
       ggplot2::ylab('Częstotliwość [%]') + 
       ggplot2::theme(legend.position = "right",
-                     axis.text = ggplot2::element_text(size = 15),
                      panel.background = ggplot2::element_rect(fill = "white"),
                      panel.grid.major = ggplot2::element_line(colour = "#D1D1C1"),
                      legend.key = ggplot2::element_rect(fill = "white"))+
