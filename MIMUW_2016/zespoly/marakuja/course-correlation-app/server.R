899afa01868fa6c895f0f5de50bdc24384b2587e
Anna Prochowska <panna.anna101@gmail.com>
2016-06-09 13:10:42 +0200

    Plot tab1 colours

--- MIMUW_2016/zespoly/marakuja/course-correlation-app/server.R
+++ MIMUW_2016/zespoly/marakuja/course-correlation-app/server.R
@@ -114,8 +114,9 @@ shinyServer(function(input, output, session) {
       geom_text(aes(label=`Procent studentów, którzy uzyskali co najmniej wybraną ocenę`), position=position_dodge(width=0.9), vjust=-0.25) +
       xlab('Przedmiot A') +
       ylab('') +
-      scale_fill_discrete(name=paste("Czołówka przedmiotów z procentem osób \npowyżej oceny X"),
-                          labels=c("Najwyższym", "Najniższym")) +
+      scale_fill_manual(values = c("deepskyblue2", "hotpink2"),
+                        name=paste("Czołówka przedmiotów z procentem osób \npowyżej oceny X"),
+                        labels=c("Najwyższym", "Najniższym")) +
       geom_hline(aes(yintercept=line_value, colour=Threshold),
                  linetype="dashed",
                  data.frame(y=line_value,
