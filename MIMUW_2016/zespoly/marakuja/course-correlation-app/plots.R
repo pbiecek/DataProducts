edae902820a19b17eaf1e66e6c86e53ef113f14e
Anna Prochowska <panna.anna101@gmail.com>
2016-06-12 14:13:37 +0200

    Vertical bar labels with line breaks

--- MIMUW_2016/zespoly/marakuja/course-correlation-app/plots.R
+++ MIMUW_2016/zespoly/marakuja/course-correlation-app/plots.R
@@ -1,4 +1,5 @@
 source("utils.R")
+library(stringr)
 
 formatPlot <- function(dataFunc) {
   ggplot(dataFunc(), aes(x = ocena_przedmiot_B, y = liczba_studentow, color = warunek)) +
@@ -17,10 +18,10 @@ barPercentPlot <- function(data, direction, course, min_grade) {
                      y = `Procent studentów, którzy uzyskali co najmniej wybraną ocenę`,
                      fill=factor(type))) +
     geom_bar(stat = "identity", width=.5) +
-    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5)) +
     geom_text(aes(label=`Procent studentów, którzy uzyskali co najmniej wybraną ocenę`), position=position_dodge(width=0.9), vjust=-0.25) +
     xlab('Przedmiot A') +
     ylab('') +
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 15)) +
     scale_fill_manual(values = c("deepskyblue2", "hotpink2"),
                       name=paste("Czołówka przedmiotów z procentem osób \npowyżej oceny X"),
                       labels=c("Najwyższym", "Najniższym")) +
