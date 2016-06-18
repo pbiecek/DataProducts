6e977d9ce6382bdbd67bbede154df57540505f67
Szymon Dziewiatkowski <snonil999@gmail.com>
2016-06-18 15:45:58 +0200

    add_error refactor

--- MIMUW_2016/zespoly/marakuja/course-correlation-app/logic-tab2.R
+++ MIMUW_2016/zespoly/marakuja/course-correlation-app/logic-tab2.R
@@ -58,39 +58,31 @@ plot_percent_min_grade <- function(df, error) {
   if (all_students == 0) {
     all_students = 1
   }
-  df_summarised$conajmniej = df_summarised$liczba_studentow
-  df_summarised$procent_studentow = df_summarised$conajmniej / all_students
+  df_summarised$procent_studentow = df_summarised$liczba_studentow / all_students
   df_summarised$id = c(1:6)
-  print(df_summarised)
+  #print(df_summarised)
   df_summarised
 }
 
 add_error <- function(df) {
-  x <- df$liczba_studentow
-  maks <- x[1]
-  wyn1 <- x
-  wyn2 <- x
+  all_students <- (df$liczba_studentow)[1]
+  min_error <- numeric(6)
+  max_error <- numeric(6)
+  if(all_students != 0) {
+    min_error[1] = 1
+    max_error[1] = 1
+  }
   i <- 1
-  for(a in x) {
-    if (maks != 0)
-    {
-      b <- prop.test(a, maks)
-      wyn1[i] = b$conf.int[1]
-      wyn2[i] = b$conf.int[2]
-    } else
-    {
-      wyn1[i] = 0
-      wyn2[i] = 0
+  for(students_with_mark in df$liczba_studentow) {
+    if (i != 1) {
+      error_range <- prop.test(students_with_mark, all_students)
+      min_error[i] = error_range$conf.int[1]
+      max_error[i] = error_range$conf.int[2]
     }
     i <- i + 1
   }
-  df$min_err = wyn1
-  df$max_err = wyn2
-  df[1,3] = 1
-  if (maks == 0)
-  {
-    df[1,3] = 0
-  }
+  df$min_err = min_error
+  df$max_err = max_error
   df
 }
 
@@ -133,7 +125,7 @@ twoCoursesTable <- function(points_two_courses, course_a, course_b) {
   data$procent_studentow = percent_from_fraction(data$procent_studentow)
   data$min_err = percent_from_fraction(data$min_err)
   data$max_err = percent_from_fraction(data$max_err)
-  data <- select(data, ocena_przedmiot_B, warunek, conajmniej, procent_studentow)
+  data <- select(data, ocena_przedmiot_B, warunek, liczba_studentow, procent_studentow)
   
   filter(data, warunek == "brak") %>% arrange(-ocena_przedmiot_B) -> data_no_condition
   filter(data, warunek == "nie zdał przedmiotu A") %>% arrange(-ocena_przedmiot_B) -> data_failed
@@ -158,7 +150,7 @@ createSummary <- function(points_two_courses, course_a, course_b) {
   data %>% filter(warunek == "brak") -> data_no_condition
   data %>% filter(warunek == "nie zdał przedmiotu A") -> data_failed
   data %>% filter(warunek == "zdał przedmiot A") -> data_passed
-  table <- matrix(c(data_no_condition$conajmniej, data_failed$conajmniej, data_passed$conajmniej), ncol = 3)
+  table <- matrix(c(data_no_condition$liczba_studentow, data_failed$liczba_studentow, data_passed$liczba_studentow), ncol = 3)
   colnames(table) = c(
     "wszystkich",
     paste('którzy nie zdali przedmiotu "', course_a, '"', sep=""),
