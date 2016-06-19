5b03cd98ba7e0f41ccdd6ea7a1a0f19b59d6186a
Szymon Dziewiatkowski <snonil999@gmail.com>
2016-06-19 14:01:24 +0200

    Small configuration

--- MIMUW_2016/zespoly/marakuja/course-correlation-app/courses.R
+++ MIMUW_2016/zespoly/marakuja/course-correlation-app/courses.R
@@ -1,7 +1,9 @@
+source("config.R")
+
 library(dplyr)
 
 get_courses_vector <- function(){
-  table <- read.csv("courses.csv")
+  table <- read.csv(COURSES_LIST_PATH)
   i <- sapply(table, is.factor)
   table[i] <- lapply(table[i], as.character)
   table$courses_vector
