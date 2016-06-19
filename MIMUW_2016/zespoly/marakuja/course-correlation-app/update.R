e0f69016be539948812786774dcf275d445aec95
Szymon Dziewiatkowski <snonil999@gmail.com>
2016-06-19 15:20:12 +0200

    update.R get_data_from_database - refactor

--- MIMUW_2016/zespoly/marakuja/course-correlation-app/update.R
+++ MIMUW_2016/zespoly/marakuja/course-correlation-app/update.R
@@ -4,11 +4,12 @@ source("config.R")
 source ("utils.R")
 source ("input.R")
 library(dplyr)
+library(RJDBC)
 
 get_courses_list <- function() {
   courses_vector = system2(
       command = "python",
-      args = "listcourses.py",
+      args = "listcourses/listcourses.py",
       stdout = TRUE
       )
   df <- data.frame(courses_vector)
@@ -28,42 +29,39 @@ save_courses_list <- function(data, file) {
 #'    Ustalić wartości poniższych parametrów.
 #'    Rscript import_data.R
 
-get_data_from_DataBase <- function() {
-
-  driverClass <- "oracle.jdbc.OracleDriver"
-  classPath <- "ojdbc6.jar"
-  host <- "usos.edu.pl"
-  port <- 1521
-  serviceName <- "USOS"
-  user <- "user"
-  password <- "password"
-  outputFile <- GRADES_CSV_PATH
-
-  library(RJDBC)
-
-  codePrefixMIM <- "1000-"
-
+get_driver <- function(driverClass, classPath) {
   driver <- JDBC(driverClass=driverClass,
                  classPath=classPath,
                  identifier.quote=" ")
+  driver
+}
 
-  url <- paste("jdbc:oracle:thin:@//", host, ":", port, "/", serviceName,
+get_url <- function(host, port, service_name) {
+  url <- paste("jdbc:oracle:thin:@//", host, ":", port, "/", service_name,
                sep="")
+  url
+}
 
+create_connection <- function(driver, url, user, password) {
   connection <- dbConnect(driver, url, user, password)
+  connection
+}
 
-  pattern <- paste("'", codePrefixMIM, "%'",
-                 sep="")
-
+create_query <- function(code_prefix_MIM) {
+  pattern <- paste("'", code_prefix_MIM, "%'",
+                  sep="")
+  
   query <- paste("SELECT kod, prz_nazwa, numer_terminu, ocena_liczbowa, osoba FROM rstat_oceny ",
                  "WHERE ocena_liczbowa is not null ",
                  "AND kod LIKE ", pattern,
                  sep="")
+  query
+}
 
+get_data_from_database <- function(connection, query) {
   data <- dbGetQuery(connection, query)
   data
 }
-#write.csv(data, outputFile)
 
 get_first_grade_for_courses <- function(grades_dataset) {
   grades_dataset %>%
@@ -88,13 +86,26 @@ save_filtered_dataset <- function(data, file) {
   write.csv(data, file)
 }
 
-#Code to be executed
+
 grades_file <- PROCESSED_GRADES_CSV_PATH
 courses_file <- COURSES_LIST_PATH
+driver_class <- DRIVER_CLASS
+driver_class_path <- DRIVER_CLASS_PATH
+host <- HOST
+port <- PORT
+service_name <- SERVICE_NAME
+user <- USER
+password <- PASSWORD
+code_prefix_MIM <- CODE_PREFIX
 
 get_courses_list() %>%
   save_courses_list(courses_file)
 
-#get_data_from_DataBase() %>% 
-#  get_filtered_dataset() %>%
-#  save_filtered_dataset(grades_file)
+url <- get_url(host, port, service_name)
+driver <- get_driver(driver_class, driver_class_path)
+connection <- create_connection(driver, url, user, password)
+query <- create_query(code_prefix_MIM)
+
+get_data_from_database(connection, query) %>% 
+  get_filtered_dataset() %>%
+  save_filtered_dataset(grades_file)
