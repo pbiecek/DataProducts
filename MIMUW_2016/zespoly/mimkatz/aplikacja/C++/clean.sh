1d24013cfe4d7c8825fedf1b7d26b0392e4fd84c
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-07-19 22:46:33 +0200

    mimkatz kopia

--- MIMUW_2016/zespoly/mimkatz/aplikacja/C++/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gradesInPath=""
-gradesOutPath=""
-questionsInPath=""
-questionsOutPath=""
-which="both" #inne możliwe wartości to "grades" lub "questions"
-
-./jnpcleaner $which $gradesInPath $gradesOutPath $questionsInPath $questionsOutPath;
--- MIMUW_2016/zespoly/mimkatz/aplikacja/SQL/data.sql
+++
@@ -1,7 +0,0 @@
-set linesize 1000;
-spool /home/staff/mim/mk305152/jnp2/bd/ankiety_raw.txt;
-select KOD, PRZ_NAZWA, ZAJ_KOD, PROWADZACY, WARTOSC_ODPOWIEDZI, LICZBA_ODPOWIEDZI, TRESC_PYTANIA, CDYD_KOD from rstat_czestosci_ankiet where KOD like '%1000-%';
-spool off;
-spool /home/staff/mim/mk305152/jnp2/bd/oceny_raw.txt;
-select KOD, PRZ_NAZWA, CDYD_KOD, NUMER_TERMINU, OCENA, OSOBA from rstat_oceny where jed_org_kod_biorca=10000000;
-spool off;
